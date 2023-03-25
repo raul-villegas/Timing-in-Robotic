d490d43c73f975de3418b9be2d288c270584e320
raul-villegas <127860226+raul-villegas@users.noreply.github.com>
2023-03-25 22:14:12 +0100

    Update myprogram.cpp

--- myprogram.cpp
+++ myprogram.cpp
@@ -1,44 +1,62 @@
 #include <iostream>
-#include <cmath>
-#include <unistd.h>
+#include <chrono>
+#include <thread>
 #include <time.h>
-#include <alchemy/task.h>
+#include <native/task.h>
+#include <native/timer.h>
 
-#define PERIOD_NS 1000000   // 1 ms period
-
-RT_TASK task;   // Xenomai real-time task
+#define TASK_PERIOD 1000000 // 1 ms
 
 void periodic_task(void *)
 {
-    timespec next_time;
-    clock_gettime(CLOCK_MONOTONIC, &next_time);
+    RT_TASK task;
+    rt_task_create(&task, "periodic_task", 0, 50, 0);
+    rt_task_set_periodic(&task, TM_NOW, TASK_PERIOD);
 
-    for (int i = 0; i < 10000000; i++)
+    rt_printf("Starting periodic task\n");
+
+    const int num_iterations = 10000000;
+    double iteration_durations[num_iterations];
+
+    timespec prev_end_time;
+    clock_gettime(CLOCK_MONOTONIC, &prev_end_time);
+
+    for (int i = 0; i < num_iterations; i++)
     {
         // Do some computational work
-        double x = sin(i);
+        double x = i * i;
+
+        timespec iteration_end_time;
+        clock_gettime(CLOCK_MONOTONIC, &iteration_end_time);
+
+        double iteration_duration = (iteration_end_time.tv_sec - prev_end_time.tv_sec) * 1000.0 + (iteration_end_time.tv_nsec - prev_end_time.tv_nsec) / 1000000.0;
+
+        iteration_durations[i] = iteration_duration;
+
+        prev_end_time = iteration_end_time;
 
         // Sleep until the next execution time
-        next_time.tv_nsec += PERIOD_NS;
-        if (next_time.tv_nsec >= 1000000000)
-        {
-            next_time.tv_sec += 1;
-            next_time.tv_nsec -= 1000000000;
-        }
-        clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &next_time, NULL);
+        rt_task_wait_period(NULL);
     }
+
+    rt_printf("Periodic task finished\n");
+
+    // Print the iteration durations
+    for (int i = 0; i < num_iterations; i++)
+    {
+        rt_printf("Iteration %d duration: %f ms\n", i, iteration_durations[i]);
+    }
+
+    rt_task_delete(&task);
 }
 
 int main()
 {
-    // Create a Xenomai real-time task with default priority and mode
-    rt_task_create(&task, "periodic_task", 0, 50, T_JOINABLE);
-
-    // Start the task
+    RT_TASK task;
+    rt_task_create(&task, "main_task", 0, 99, 0);
     rt_task_start(&task, &periodic_task, NULL);
-
-    // Wait for the task to finish
     rt_task_join(&task);
+    rt_task_delete(&task);
 
     return 0;
 }
